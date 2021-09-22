cc91f2fd7a5afdaffb48ed72cfc59707143aed9e
LastSkywalker <maxdr1998@gmail.com>
2021-09-22 22:34:46 +0300

    add some interface

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+'use strict';
 
-const Greetings = () => {
+import React, {Suspense, useEffect, useState} from 'react';
+
+const Table = ({data}) => data.map((element, i) => (
+      <div key={i} className="row">
+        <span>
+          {element.symbol}
+        </span>
+        <span>
+          {element.sector}
+        </span>
+        <span>
+          {element.price}
+        </span>
+        <span>
+          {element.currency}
+        </span>
+      </div>
+    ))
+
+const Greetings = () => {  
+
+  const [data, updateData] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('../static/db/table.json');
+      const json = await response.json();
+      updateData(json);
+    }
+
+    getData();
+  }, [])
+
   return (
-  <div className="greetings">
-    Hi there
-  </div>
-  )
+    <div className="greetings">
+      <Suspense fallback={<h1>Loading...</h1>}> 
+        {data && <Table data={data}/>}
+      </Suspense>
+    </div>
+    )
 }
 
 export default Greetings;
