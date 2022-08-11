a8cef2a3eea32ed4d0f9645c81951b381038b187
LastSkywalkerER <maxdr1998@gmail.com>
2022-08-11 11:06:37 +0300

    add ts-node and prepare for heroku

--- server/app.ts
+++ server/app.ts
@@ -1,34 +1,34 @@
-import express from 'express'
-import config from 'config'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import WebSocket from 'ws'
-import http from 'http'
-import path from 'path'
+import express from 'express';
+import config from 'config';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import WebSocket from 'ws';
+import http from 'http';
+import path from 'path';
 
-import authRoutes from './routes/auth.routes'
-import tickersRoutes from './routes/stock.routes'
-import tableRoutes from './routes/table.routes'
-import './telegramBot'
+import authRoutes from './routes/auth.routes';
+import tickersRoutes from './routes/stock.routes';
+import tableRoutes from './routes/table.routes';
+import './telegramBot';
 
-import onMessage, { broadcast } from './webSocket/on.message'
-import { wsPackageTypes } from './types/wsPackageTypes'
-import { Scrap } from './utils/scrap'
+import onMessage, { broadcast } from './webSocket/on.message';
+import { wsPackageTypes } from './types/wsPackageTypes';
+import { Scrap } from './utils/scrap';
 
-const app = express()
-const PORT = config.get('port') || 5000
-const server = http.createServer(app)
-const webSocketServer = new WebSocket.Server({ server })
+const app = express();
+const PORT = config.get('port') || 5000;
+const server = http.createServer(app);
+const webSocketServer = new WebSocket.Server({ server });
 
 // говорим экспрессу, что можно принимать запросы с чужих доменов
-app.use(cors())
+app.use(cors());
 
 // говорим экспрессу, что ему надо работать с json
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/auth', authRoutes)
-app.use('/api/tickers', tickersRoutes)
-app.use('/api/table', tableRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/tickers', tickersRoutes);
+app.use('/api/table', tableRoutes);
 
 webSocketServer.on('connection', (ws) => {
   webSocketServer.clients.forEach((client) =>
@@ -38,48 +38,48 @@ webSocketServer.on('connection', (ws) => {
         data: 'Somebody connected',
       }),
     ),
-  )
+  );
   ws.on('message', (msg) => {
-    onMessage(msg, webSocketServer, ws)
-  })
-})
+    onMessage(msg, webSocketServer, ws);
+  });
+});
 
 setInterval(() => {
-  const date = new Date()
+  const date = new Date();
 
   if (date.getHours() === 10 && date.getMinutes() === 50) {
-    const scrap = new Scrap()
+    const scrap = new Scrap();
 
-    scrap.run((data) => {
+    scrap.run((data: Record<string, unknown>) => {
       broadcast(
         webSocketServer,
         JSON.stringify({
           type: wsPackageTypes.TABLE_UPDATE_REQUEST,
           data,
         }),
-      )
-    })
+      );
+    });
   }
-}, 60000)
+}, 60000);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, '..', 'dist')))
+  app.use('/', express.static(path.join(__dirname, '..', 'client', 'build')));
 
   app.get('*', (request, response) => {
-    response.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'))
-  })
+    response.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+  });
 }
 
 async function start() {
   try {
-    await mongoose.connect(config.get('mongoUri'))
+    await mongoose.connect(config.get('mongoUri'));
     server.listen(PORT, () => {
-      console.log(`App started at ${PORT}`)
-    })
+      console.log(`App started at ${PORT}`);
+    });
   } catch (e: any) {
-    console.log('Server Error', e.message)
-    process.exit(1)
+    console.log('Server Error', e.message);
+    process.exit(1);
   }
 }
 
-start()
+start();
